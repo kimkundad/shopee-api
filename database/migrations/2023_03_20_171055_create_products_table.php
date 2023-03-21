ca82a24efd04b86a3f58094eae316593851dc9e8
kimkundad <kim.kundad@gmail.com>
2023-03-21 09:29:54 +0700

    kim kundad

---
+++ database/migrations/2023_03_20_171055_create_products_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_product')->nullable();
+            $table->text('detail_product')->nullable();
+            $table->string('img_product')->nullable();
+            $table->double('cost', 15, 2)->default('0.0');
+            $table->double('price', 15, 2)->default('0.0');
+            $table->double('ratting', 15, 2)->default('0.0');
+            $table->string('category')->nullable();
+            $table->integer('price_sales')->nullable();
+            $table->integer('stock')->nullable();
+            $table->string('weight')->nullable();
+            $table->string('width_product')->nullable();
+            $table->string('length_product')->nullable();
+            $table->string('height_product')->nullable();
+            $table->string('user_code')->nullable();
+            $table->integer('type')->default(0);
+            $table->integer('active')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
