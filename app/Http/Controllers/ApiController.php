1f7cda68ebc178047791f8f304ae81fa6a2d8b00
kimkundad <kim.kundad@gmail.com>
2023-03-30 14:08:15 +0700

    kim

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -43,7 +43,7 @@ class ApiController extends Controller
     {
         $data = $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'phone_number' => ['required', 'numeric', 'unique:users'],
+            'phone' => ['required', 'numeric', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
         /* Get credentials from .env */
@@ -53,13 +53,13 @@ class ApiController extends Controller
         $twilio = new Client($twilio_sid, $token);
         $twilio->verify->v2->services($twilio_verify_sid)
             ->verifications
-            ->create($data['phone_number'], "sms");
+            ->create($data['phone'], "sms");
         User::create([
             'name' => $data['name'],
-            'phone' => $data['phone_number'],
+            'phone' => $data['phone'],
             'password' => Hash::make($data['password']),
         ]);
-        return redirect()->route('verify')->with(['phone_number' => $data['phone_number']]);
+        return redirect()->route('verify')->with(['phone' => $data['phone']]);
     }
 
     public function get_allproduct()
