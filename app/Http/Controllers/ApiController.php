9b79dd94faa59aaaf515f740288f2d874b0570f4
BryantzTH <71428400+bryantz13@users.noreply.github.com>
2023-04-03 16:13:30 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -358,7 +358,7 @@ class ApiController extends Controller
 
     public function getAllUsers()
     {
-        $objs = DB::table('users')->select('users.*', 'roles.name as role_name', 'users.name as user_name', 'users.created_at as user_created_at')
+        $objs = DB::table('users')->select('users.*', 'users.id as userID', 'roles.name as role_name', 'users.name as user_name', 'users.created_at as user_created_at')
         ->join('role_user', 'role_user.user_id', '=', 'users.id')
         ->join('roles', 'roles.id', '=', 'role_user.role_id')
         ->get();
