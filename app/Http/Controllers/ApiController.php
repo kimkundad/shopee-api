37af8f7995d9be04010bb38e5f9431591192a42a
infree002 <piyachat.sng@gmail.com>
2023-04-07 15:52:30 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -379,7 +379,7 @@ class ApiController extends Controller
         } */
         $dataOption = json_decode($request->dataOption, true);
         return response()->json([
-            'product' => $dataOption,
+            'product' => $dataOption[0]->indexImageOption1,
         ], 201);
     }
 
