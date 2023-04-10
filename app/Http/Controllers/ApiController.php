a602d9055c0af8036b5a35230d9ddcbce5d15fcb
BryantzTH <71428400+bryantz13@users.noreply.github.com>
2023-04-10 11:28:37 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -698,7 +698,6 @@ class ApiController extends Controller
     // ---------------------------------ฟังก์ชันสร้างร้านค้า create by อั้นเอง----------------------------
     public function createShop(Request $request)
     {
-        dd($request);
         if ($request->hasFile('file') && $request->hasFile('file2')) {
             $files = $request->file('file');
             $filePaths = null;
@@ -737,12 +736,12 @@ class ApiController extends Controller
             // รับค่า ID ของแถวที่เพิ่งถูกเพิ่มล่าสุดเข้าไปในตาราง users
             $lastInsertId = DB::getPdo()->lastInsertId();
 
-            if ($request->input('selectProduct')) {
-                $selectedProducts = $request->input('selectProduct');
+            if ($request->input('selectID')) {
+                $selectedProducts = $request->input('selectID');
                 if (is_array($selectedProducts)) {
                     foreach ($selectedProducts as $productId) {
                         DB::table('shop_list_products')->insert([
-                            'product_id' => $productId['id'],
+                            'product_id' => $productId,
                             'shop_id' => $lastInsertId,
                             'created_at' =>  date('Y-m-d H:i:s'),
                             'updated_at' => date('Y-m-d H:i:s'),
