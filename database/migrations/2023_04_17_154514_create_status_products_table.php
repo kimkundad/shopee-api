84a40aaf730406f8334052928cd4337259c58aa2
infree002 <piyachat.sng@gmail.com>
2023-04-17 15:48:52 +0700

    Update 2023_04_17_154514_create_status_products_table.php

--- database/migrations/2023_04_17_154514_create_status_products_table.php
+++ database/migrations/2023_04_17_154514_create_status_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('status_products', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
             $table->integer('shop_id');
             $table->integer('product_id');
             $table->integer('product_option_id');
