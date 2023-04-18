e560012757a4c9829ceaddc3c76cf378d2fec6f6
BryantzTH <71428400+bryantz13@users.noreply.github.com>
2023-04-18 16:13:45 +0700

    add table migration master data theme shop

---
+++ database/migrations/2023_04_18_160953_create_themes_shop.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('themes_shop', function (Blueprint $table) {
+            $table->id();
+            $table->integer('theme_code')->default(0);
+            $table->text('theme_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('themes_shop');
+    }
+};
