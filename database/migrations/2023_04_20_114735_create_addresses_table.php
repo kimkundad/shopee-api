2dda9308ba14cebb4b5c7b4c33a1456dfaada679
infree002 <piyachat.sng@gmail.com>
2023-04-20 16:08:26 +0700

    update

--- database/migrations/2023_04_20_114735_create_addresses_table.php
+++ database/migrations/2023_04_20_114735_create_addresses_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('address', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
             $table->text('name');
@@ -34,6 +34,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('address');
+        Schema::dropIfExists('addresses');
     }
 };
