8dc0edbab6368af0784ee2f1762639eed3622b78
infree002 <piyachat.sng@gmail.com>
2023-04-24 09:31:26 +0700

    update db bank

---
+++ database/migrations/2023_04_24_092638_create_bankaccounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bankaccounts', function (Blueprint $table) {
+            $table->id();
+            $table->int('user_id');
+            $table->int('bank_id');
+            $table->text('bankaccount_number');
+            $table->text('bankaccount_name');
+            $table->text('QR_code');
+            $table->int('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bankaccounts');
+    }
+};
