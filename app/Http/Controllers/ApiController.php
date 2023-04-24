fdc0c0cd58ac2dc1c9da8193a4d1f07c0b747df8
infree002 <piyachat.sng@gmail.com>
2023-04-24 14:57:11 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -917,7 +917,7 @@ class ApiController extends Controller
     // ดึงข้อมูลธนาคาร
     public function getBankaccount(Request $request) {
         if($request->bank_id !== null){
-            $banks = DB::table('bankaccounts')->leftjoin('banks','banks.id','=','bankaccounts.bank_id')->where('bankaccounts.id','=',$request->bank_id)->where('bankaccounts.is_active','=',1)->first();
+            $banks = DB::table('bankaccounts')->leftjoin('banks','banks.id','=','bankaccounts.bank_id')->where('bankaccounts.id','=',$request->id)->where('bankaccounts.is_active','=',1)->first();
 
             return response()->json([
                 'banks' => $banks,
