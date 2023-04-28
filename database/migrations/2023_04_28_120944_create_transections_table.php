32edac11a2624926dab6482e57962fc4bdc7197e
infree002 <piyachat.sng@gmail.com>
2023-04-28 12:17:34 +0700

    update db transections

---
+++ database/migrations/2023_04_28_120944_create_transections_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transections', function (Blueprint $table) {
+            $table->id();
+            $table->text('slip');
+            $table->int('order_id');
+            $table->data('date');
+            $table->time('time');
+            $table->text('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transections');
+    }
+};
