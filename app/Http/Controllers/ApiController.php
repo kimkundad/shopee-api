26dcd2f718f7c8a2e7fbd2d26a42372da004d55a
infree002 <piyachat.sng@gmail.com>
2023-05-02 11:30:06 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -923,7 +923,7 @@ class ApiController extends Controller
             ->join('shops', 'shops.id', '=', 'chats.shop_id')
             ->where('chats.shop_id','=', $request->shop_id)
             ->where('chats.sender_id', '=', $request->user_id)
-            ->orWhere('chats.recived_id', '=', $request->user_id)
+            /* ->orWhere('chats.recived_id', '=', $request->user_id) */
             ->orderBy('chats.created_at', 'asc')
             ->select([
                 'chats.*',
