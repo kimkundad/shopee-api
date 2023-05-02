c9ccddc1e87ac0d7d9446c358091dc03b5a15c52
infree002 <piyachat.sng@gmail.com>
2023-05-02 12:32:27 +0700

    Update 2023_04_17_154514_create_orders_table.php

--- database/migrations/2023_04_17_154514_create_orders_table.php
+++ database/migrations/2023_04_17_154514_create_orders_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
+            $table->integer('address_id');
             $table->integer('shop_id');
             $table->integer('order_detail_id');
             $table->integer('num');
