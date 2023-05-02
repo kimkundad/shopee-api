417c84068e40eb51271bf0d334407e19a0674eff
infree002 <piyachat.sng@gmail.com>
2023-05-02 11:17:27 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -921,7 +921,7 @@ class ApiController extends Controller
         $message = DB::table('chats')
             ->join('users', 'users.id', '=', 'chats.user_id')
             ->join('shops', 'shops.id', '=', 'chats.shop_id')
-            ->where('chats.shop_id', $request->shop_id)
+            ->where('chats.shop_id','=', $request->shop_id)
             ->where('chats.sender_id', '=', $request->user_id)
             ->orWhere('chats.recived_id', '=', $request->user_id)
             ->orderBy('chats.created_at', 'asc')
