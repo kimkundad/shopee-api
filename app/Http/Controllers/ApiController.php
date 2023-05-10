fb7498eb9c5856f9d566feed9997b06104911147
BryantzTH <71428400+bryantz13@users.noreply.github.com>
2023-05-10 09:49:07 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2070,7 +2070,8 @@ class ApiController extends Controller
                 'orders.updated_at as updateAt',
                 'orders.status as status',
                 'orders.type_payment as typePaymentOrder',
-                'transections.slip as slipPayment'
+                'transections.slip as slipPayment',
+                'bankaccounts.bankaccount_number as accountNumber'
             ])
             ->get();
         foreach ($orders2 as $value) {
