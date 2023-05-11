32464eef2722a7bc6c28446e216ee3d5a88d9dbc
infree002 <piyachat.sng@gmail.com>
2023-05-11 12:21:42 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -195,7 +195,8 @@ class ApiController extends Controller
                             'product_suboptions.sub_op_name' => 'sub_op_name',
                             'product_suboptions.price AS price_type_3',
                         ])
-                        ->whereIn('shops.id', $item->id)
+                        ->whereIn('carts.id', $carts_id)
+                        ->where('shops.id','=',$item->id)
                         ->get();
                     return $item;
                 });
