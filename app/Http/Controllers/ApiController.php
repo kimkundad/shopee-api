f1d2f8c549fb55caebe72112640b4324f7d04d31
infree002 <piyachat.sng@gmail.com>
2023-05-11 12:14:45 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -172,6 +172,7 @@ class ApiController extends Controller
                 ->map(function ($item) use ($carts_id) {
                     $item->product = DB::table('carts')
                         ->join('shop_list_products', 'shop_list_products.shop_id', '=', 'carts.shop_id')
+                        ->join('shops', 'carts.shop_id', '=', 'shops.id')
                         ->join('products', 'products.id', '=', 'carts.product_id')
                         ->leftjoin('product_options', 'product_options.id', '=', 'carts.product_options_id')
                         ->leftjoin('product_suboptions', 'product_suboptions.id', '=', 'carts.product_suboptions_id')
