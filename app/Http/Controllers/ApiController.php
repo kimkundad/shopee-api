7da89c445a4c4d83ddfc416b94931da4c05ba9e1
infree002 <piyachat.sng@gmail.com>
2023-05-12 09:40:31 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -415,7 +415,7 @@ class ApiController extends Controller
                     ->get()
                     ->map(function ($subItem) {
                         $subItem->products = DB::table('order_details')
-                            -/* >leftjoin('products', 'products.id', '=', 'order_details.product_id')
+                            /* ->leftjoin('products', 'products.id', '=', 'order_details.product_id')
                             ->leftjoin('product_options', 'product_options.id', '=', 'order_details.option1')
                             ->leftjoin('product_suboptions', 'product_suboptions.id', '=', 'order_details.option2')
                             ->select([
