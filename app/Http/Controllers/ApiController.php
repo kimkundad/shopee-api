f837ead12ff95bbcf55851e0476429853f2031a1
BryantzTH <71428400+bryantz13@users.noreply.github.com>
2023-05-17 09:49:46 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2080,7 +2080,7 @@ class ApiController extends Controller
         ], 201);
     }
 
-    public function getOrders()
+    public function getOrders(Request $request)
     {
         // $orders2 = DB::table('orders')
         //     ->leftjoin('order_details', 'orders.id', '=', 'order_details.order_id')
@@ -2135,7 +2135,8 @@ class ApiController extends Controller
                 'transections.date as dateSlipPayment',
                 'transections.time as timeSlipPayment',
             ])
-            ->get();
+            ->where('orders.status', $request->navbarTab)
+            ->paginate(5);
         foreach ($orders2 as $value) {
             $data = DB::table('order_details')
                 ->leftJoin('products', 'products.id', '=', 'order_details.product_id')
