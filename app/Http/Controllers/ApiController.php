f23dfe8be2578df751494a776b673d0fa5b795d4
infree002 <piyachat.sng@gmail.com>
2023-05-22 11:23:04 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1447,7 +1447,7 @@ class ApiController extends Controller
 
     // profile user
     public function getOwnershops(Request $request){
-         $obj = DB::table('ownershops')->where('user_code','=',$request->uid)->first();
+         $obj = DB::table('ownershops')->where('user_code','=',$request->user_code)->first();
 
          return response()->json([
             'owner_shop' => $obj,
