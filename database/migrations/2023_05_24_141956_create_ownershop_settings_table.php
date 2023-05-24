a269396094726e96b0840ce0b8493523ee397759
infree002 <piyachat.sng@gmail.com>
2023-05-24 14:23:10 +0700

    update database table ownersetting

---
+++ database/migrations/2023_05_24_141956_create_ownershop_settings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ownershop_settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_code');
+            $table->string('setting');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ownershop_settings');
+    }
+};
