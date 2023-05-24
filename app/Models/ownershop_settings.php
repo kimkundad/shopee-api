e30a8bee914840376b06eda26b5d4b52749199fe
infree002 <piyachat.sng@gmail.com>
2023-05-24 16:21:54 +0700

    Update ownershop_settings.php

--- app/Models/ownershop_settings.php
+++ app/Models/ownershop_settings.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ownershop_setting extends Model
+class ownershop_settings extends Model
 {
     use HasFactory;
 }
