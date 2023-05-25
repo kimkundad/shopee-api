bc087c95a106cbe4e9e05bd59a11cdafcf45b20e
infree002 <piyachat.sng@gmail.com>
2023-05-25 12:09:46 +0700

    update db notification

---
+++ database/migrations/2023_05_25_120159_create_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_code');
+            $table->integer('user_id');
+            $table->integer('order_id');
+            $table->string('type_noti');
+            $table->string('detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
