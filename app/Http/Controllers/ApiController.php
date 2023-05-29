b8f1d6676717f25a03f9d8bb14e5baab1fd57035
BryantzTH <71428400+bryantz13@users.noreply.github.com>
2023-05-29 11:09:07 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2874,7 +2874,9 @@ class ApiController extends Controller
     {
         $option_id = $request->productSelect;
         foreach ($option_id as $index => $value) {
-            DB::table('product_suboptions')->where('op_id', $value)->where('sub_op_name', $request->removedText)->delete();
+            if ($value) {
+                DB::table('product_suboptions')->where('op_id', $value)->where('sub_op_name', $request->removedText)->delete();
+            }
         }
         return response()->json([
             'success' => 'Deleted sub option successfully',
