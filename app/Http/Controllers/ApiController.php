93073d27d84c8f9864ccf2696f2ac33e6b41c64a
BryantzTH <71428400+bryantz13@users.noreply.github.com>
2023-06-20 16:32:01 +0700

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2777,6 +2777,8 @@ class ApiController extends Controller
         //     ->get();
         $search = $request->search;
         $searchDate = $request->searchDate;
+        $ucode = $request->user_code;
+
         $orders2 = DB::table('orders')
             ->leftJoin('addresses', 'addresses.id', '=', 'orders.address_id')
             ->leftJoin('transections', 'transections.order_id', '=', 'orders.id')
@@ -2808,9 +2810,9 @@ class ApiController extends Controller
             ])
             ->where('orders.user_code', $request->user_code)
             ->where('orders.status', $request->navbarTab)
-            ->where(function ($query) use ($search, $searchDate) {
+            ->where(function ($query) use ($search, $searchDate, $ucode) {
                 if (!empty($searchDate) && empty($search)) {
-                    $query->whereDate('orders.created_at', $searchDate);
+                    $query->whereDate('orders.created_at', $searchDate)->where('orders.user_code', $ucode);
                 } elseif (empty($searchDate) && !empty($search)) {
                     $query->where(function ($query) use ($search) {
                         $query->orWhere('orders.invoice_id', 'like', '%' . $search . '%')
@@ -2818,15 +2820,15 @@ class ApiController extends Controller
                             ->orWhere('addresses.address', 'like', '%' . $search . '%')
                             ->orWhere('addresses.tel', 'like', '%' . $search . '%')
                             ->orWhere('orders.price', 'like', '%' . $search . '%');
-                    });
+                    })->where('orders.user_code', $ucode);
                 } elseif (!empty($searchDate) && !empty($search)) {
-                    $query->where(function ($query) use ($search, $searchDate) {
+                    $query->where(function ($query) use ($search) {
                         $query->orWhere('orders.invoice_id', 'like', '%' . $search . '%')
                             ->orWhere('addresses.name', 'like', '%' . $search . '%')
                             ->orWhere('addresses.address', 'like', '%' . $search . '%')
                             ->orWhere('addresses.tel', 'like', '%' . $search . '%')
                             ->orWhere('orders.price', 'like', '%' . $search . '%');
-                    })->whereDate('orders.created_at', $searchDate);
+                    })->whereDate('orders.created_at', $searchDate)->where('orders.user_code', $ucode);
                 }
             })
             ->paginate($request->numShowItems);
